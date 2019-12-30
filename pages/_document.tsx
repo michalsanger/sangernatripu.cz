3b5405cec402ffaa3592298702ac46351ab74e68
Michal Sanger <michal.sanger@gmail.com>
2019-12-29 20:23:06 -1000

    Basic blog layout

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="/foundation.min.css" />
+          <style jsx global>{`
+            a {
+              text-decoration: underline;
+            }
+          `}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,29 @@ import React from "react";
 import Link from "next/link";
 
 const index = () => (
-  <div>
-    <h1>Sänger na tripu</h1>
-    <hr />
-    <Link href="2019-12-28-test-article">
-      <a>
-        <h2>Sample article</h2>
-      </a>
-    </Link>
-    <p>
-      Morbi ut purus eget velit suscipit fermentum. Aliquam dictum neque ut magna varius eleifend.
-      Suspendisse lorem sem, lobortis nec rutrum ac, efficitur non dui. Ut convallis magna eu eros
-      luctus, in aliquam risus suscipit. Duis dignissim nibh sed nulla accumsan pellentesque. Donec
-      ultricies mi sapien
-    </p>
-  </div>
+  <>
+    <div className="callout large primary">
+      <div className="row column text-center">
+        <h1>Sänger na tripu</h1>
+      </div>
+    </div>
+
+    <div className="row medium-8 large-7 columns">
+      <div className="blog-post">
+        <Link href="2019-12-28-test-article">
+          <a>
+            <h3>Sample article</h3>
+          </a>
+        </Link>
+        <p>
+          Morbi ut purus eget velit suscipit fermentum. Aliquam dictum neque ut magna varius
+          eleifend. Suspendisse lorem sem, lobortis nec rutrum ac, efficitur non dui. Ut convallis
+          magna eu eros luctus, in aliquam risus suscipit. Duis dignissim nibh sed nulla accumsan
+          pellentesque. Donec ultricies mi sapien
+        </p>
+      </div>
+    </div>
+  </>
 );
 
 export default index;
