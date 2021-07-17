858f30aff78a5e6bcb40d152e249d24472283e15
Michal Sanger <michal.sanger@gmail.com>
2021-07-17 12:56:10 +0200

    Favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "favicon.ico",
   projectName: "sangernatripu.cz", // Usually your repo name.
   i18n: {
     defaultLocale: "cs",
