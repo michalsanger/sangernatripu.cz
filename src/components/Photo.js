574468785b9ea8bdc52bc339580a79bc96c2af55
Michal Sanger <michal.sanger@gmail.com>
2021-07-17 18:31:03 +0200

    Setup photos

---
+++ src/components/Photo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "@theme/IdealImage";
+
+export default function Photo({ photo }) {
+  const isVertical = photo.src.width < photo.src.height;
+
+  const img = (
+    <Image
+      img={photo}
+      theme={{
+        placeholder: {
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "relative",
+        },
+        img: isVertical
+          ? { "object-fit": "contain", "max-height": "500px" }
+          : { "object-fit": "cover" },
+      }}
+    />
+  );
+
+  return (
+    <div className="blogPostPhoto">
+      <a href={img.props.img.src.src}>{img}</a>
+    </div>
+  );
+}
